dc7e510df540fde12c35f86b7c653841bf776164
treyzania <treyzania@gmail.com>
2018-04-18 21:18:55 -0400

    Set up cross-compiler automatic building, via submodule.

---
+++ crossbuild.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+	echo 'must specify an architecture!'
+	exit 1
+fi
+
+cd toolchain
+
+bpath=$(realpath crossbuild)
+mkdir -p $bpath
+
+pushd crosscompiler
+./doit -f -a "$1" -o $bpath
